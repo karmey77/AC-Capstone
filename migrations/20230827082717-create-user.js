b26dc4c4ee1434e238f8f498581335236bb5c16c
Yi-Hsiu Lee <67046103+karmey77@users.noreply.github.com>
2023-08-27 16:49:24 +0800

    establish SQL connections and build user model

---
+++ migrations/20230827082717-create-user.js
@@ -0,0 +1,57 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      realName: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      nickname: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      avartar: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      introduction: {
+        allowNull: true,
+        type: Sequelize.TEXT
+      },
+      isAdmin: {
+        allowNull: false,
+        type: Sequelize.BOOLEAN
+      },
+      totalLearningTime: {
+        allowNull: false,
+        type: Sequelize.FLOAT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  }
+};
