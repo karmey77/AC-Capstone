7cff4d633088ba602362aa995d6a1cbb022a43a7
Yi-Hsiu Lee <67046103+karmey77@users.noreply.github.com>
2023-09-10 19:33:46 +0800

    fix bug

--- controllers/pages/rating-controller.js
+++ controllers/pages/rating-controller.js
@@ -2,7 +2,7 @@ const ratingServices = require('../../services/rating-services')
 
 const ratingController = {
   postRating: (req, res, next) => {
-    ratingServices.postRegisteration(req, (err, data) => {
+    ratingServices.postRating(req, (err, data) => {
       if (err) return next(err)
       console.log(data.registration)
       req.session.createdData = data
