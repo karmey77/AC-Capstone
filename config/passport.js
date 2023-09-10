a7ed8010668e1a7cdd7a01cb79d514de3948189c
Yi-Hsiu Lee <67046103+karmey77@users.noreply.github.com>
2023-09-10 17:58:52 +0800

    add callback for heroku

--- config/passport.js
+++ config/passport.js
@@ -30,7 +30,7 @@ passport.use(new LocalStrategy(
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: 'http://localhost:3000/auth/google/callback'
+  callbackURL: process.env.GOOGLE_CALL_BACK
 },
 function (accessToken, refreshToken, profile, cb) {
   const { name, email, picture, locale } = profile._json
